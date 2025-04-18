eefd3ea7b9eb63d4d1ce5e8b229f69d817b9d5cf
SergeyIT24 <seregait@vk.com>
2025-04-18 15:02:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+class SimpleChat
+{
+    static async Task ConnectAsync()
+    {
+        using (ClientWebSocket client = new ClientWebSocket())
+        {
+            try
+            {
+                //ввод uri адреса
+                string uri;
+                Console.Write("Введите URI: ");
+                uri = Console.ReadLine();
+
+                //подключение к серверу
+                await client.ConnectAsync(new Uri(uri), CancellationToken.None);
+                Console.WriteLine("Соединение с сервером успешно установлено");
+
+                Console.Write("Введите сообщение: ");
+                string message = Console.ReadLine();
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                await client.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                Console.WriteLine($"Отправлено: {message}");
+
+                byte[] receiveBuffer = new byte[1024];
+                WebSocketReceiveResult result = await client.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+                string response = Encoding.UTF8.GetString(receiveBuffer);
+                Console.WriteLine($"Ответ от сервера: {response}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Возникла ошибка: {ex.Message}");
+            }
+            finally
+            {
+                if(client.State == WebSocketState.Open)
+                {
+                    await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Закрытие клиента", CancellationToken.None);
+                }
+                Console.WriteLine("Соединение успешно закрыто!");
+            }
+        }
+    }
+
+    static async Task Main()
+    {
+        await ConnectAsync();
+    }
+}
